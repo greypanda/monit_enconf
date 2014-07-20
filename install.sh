89d0876019cb9673de3fd7e3779b8f6afb8be36c
Фукалов Семен <yapendalff@trioris.com>
2014-07-20 09:16:49 +0700

    Add ubuntu install.sh script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CURR_DIR=`pwd`/
+cd $CURR_DIR
+if [ "$(whoami)" != 'root' ]; then
+   echo "you need use it with sudo: sudo ./install.sh"
+else
+   mkdir -p /usr/local/share/man/man8/nginxensite/
+   cp ./nginx_* 		/usr/sbin
+   cp ./bash_completion.d/* 	/etc/bash_completion.d
+   cp ./man/* /usr/local/share/man/man8/nginxensite
+   echo "Success!"
+fi
+
