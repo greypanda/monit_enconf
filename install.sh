486423c0ab0c642729fbcdbc82714a3596b7e37b
John Crunk <john@pandavista.com>
2017-07-19 12:34:12 -0400

    initial

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ cd $CURR_DIR
 if [ "$(whoami)" != 'root' ]; then
    echo "you need use it with sudo: sudo ./install.sh"
 else
-   mkdir -p /usr/local/share/man/man8/nginxensite/
-   cp ./nginx_* 		/usr/sbin
+   mkdir -p /usr/local/share/man/man8/monitenconf/
+   cp ./mont_* 		/usr/sbin
    cp ./bash_completion.d/* 	/etc/bash_completion.d
-   cp ./man/* /usr/local/share/man/man8/nginxensite
+   cp ./man/* /usr/local/share/man/man8/monitenconf
    echo "Success!"
 fi
 
